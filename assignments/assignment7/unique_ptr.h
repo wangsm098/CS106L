99f7b5f4a24693742b3aaca4c4bf88e6a44efbe2
wangsm098 <wangsm098@gmail.com>
2026-04-02 20:18:25 +0800

    add lecture note 16 and assignment7

---
+++ assignments/assignment7/unique_ptr.h
@@ -0,0 +1,131 @@
+#pragma once
+
+#include <cstddef>
+#include <utility>
+
+namespace cs106l {
+
+/**
+ * @brief A smart pointer that owns an object and deletes it when it goes out of scope.
+ * @tparam T The type of the object to manage.
+ * @note This class is a simpler version of `std::unique_ptr`.
+ */
+template <typename T> class unique_ptr {
+private:
+    T* pointer;
+
+public:
+  /**
+   * @brief Constructs a new `unique_ptr` from the given pointer.
+   * @param ptr The pointer to manage.
+   * @note You should avoid using this constructor directly and instead use `make_unique()`.
+   */
+  unique_ptr(T* ptr) {
+    /* STUDENT TODO: Implement the constructor */
+        pointer = ptr;
+  }
+
+  /**
+   * @brief Constructs a new `unique_ptr` from `nullptr`.
+   */
+  unique_ptr(std::nullptr_t) {
+    /* STUDENT TODO: Implement the nullptr constructor */
+        pointer = nullptr;
+  }
+
+  /**
+   * @brief Constructs an empty `unique_ptr`.
+   * @note By default, a `unique_ptr` points to `nullptr`.
+   */
+  unique_ptr() : unique_ptr(nullptr) {}
+
+  /**
+   * @brief Dereferences a `unique_ptr` and returns a reference to the object.
+   * @return A reference to the object.
+   */
+  T& operator*() {
+    /* STUDENT TODO: Implement the dereference operator */
+        return *pointer;
+  }
+
+  /**
+   * @brief Dereferences a `unique_ptr` and returns a const reference to the object.
+   * @return A const reference to the object.
+   */
+  const T& operator*() const {
+    /* STUDENT TODO: Implement the dereference operator (const) */
+        return *pointer;
+  }
+
+  /**
+   * @brief Returns a pointer to the object managed by the `unique_ptr`.
+   * @note This allows for accessing the members of the managed object through the `->` operator.
+   * @return A pointer to the object.
+   */
+  T* operator->() {
+    /* STUDENT TODO: Implement the arrow operator */
+        return pointer;
+  }
+
+  /**
+   * @brief Returns a const pointer to the object managed by the `unique_ptr`.
+   * @note This allows for accessing the members of the managed object through the `->` operator.
+   * @return A const pointer to the object.
+   */
+  const T* operator->() const {
+    /* STUDENT TODO: Implement the arrow operator */
+        return static_cast<const T*>(pointer);
+  }
+
+  /**
+   * @brief Returns whether or not the `unique_ptr` is non-null.
+   * @note This allows us to use a `unique_ptr` inside an if-statement.
+   * @return `true` if the `unique_ptr` is non-null, `false` otherwise.
+   */
+  operator bool() const {
+    /* STUDENT TODO: Implement the boolean conversion operator */
+        return pointer == nullptr ? false : true;
+  }
+
+  /** STUDENT TODO: In the space below, do the following:
+   * - Implement a destructor
+   * - Delete the copy constructor
+   * - Delete the copy assignment operator
+   * - Implement the move constructor
+   * - Implement the move assignment operator
+   */
+    
+    ~unique_ptr() {
+        delete pointer;
+    }
+
+    unique_ptr(const unique_ptr& other) = delete;
+    unique_ptr& operator= (const unique_ptr& other) = delete;
+    
+    unique_ptr(unique_ptr&& other) : pointer(other.pointer) {
+        other.pointer = nullptr;
+    }
+
+    unique_ptr& operator= (unique_ptr&& other) {
+        if (this != &other) {
+            delete pointer;
+            pointer = other.pointer;
+            other.pointer = nullptr;
+        }
+        return *this;
+    }
+};
+
+/**
+ * @brief Creates a new unique_ptr for a type with the given arguments.
+ * @example auto ptr = make_unique<int>(5);
+ * @tparam T The type to create a unique_ptr for.
+ * @tparam Args The types of the arguments to pass to the constructor of T.
+ * @param args The arguments to pass to the constructor of T.
+ */
+template <typename T, typename... Args> 
+unique_ptr<T> make_unique(Args&&... args) {
+  return unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+}
